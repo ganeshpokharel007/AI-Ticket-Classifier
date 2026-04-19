e710fb0c9769f702b82d99a1a7d5ddbcb65ea19d
Ganesh pokharel <76983858+ganeshpokharel007@users.noreply.github.com>
2026-04-19 08:40:11 +0100

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-
+from sklearn.metrics import accuracy_score
 # Load data
 data = pd.read_csv("tickets.csv")
 
@@ -21,6 +21,11 @@ X_train, X_test, y_train, y_test = train_test_split(X_vec, y, test_size=0.2)
 model = MultinomialNB()
 model.fit(X_train, y_train)
 
+
+
+# After training
+y_pred = model.predict(X_test)
+print("Model Accuracy:", accuracy_score(y_test, y_pred))
 # Test prediction
 sample = ["My internet is very slow"]
 sample_vec = vectorizer.transform(sample)
