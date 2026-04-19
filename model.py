33288aa9d0aff46345086e207dedd08974954902
Ganesh pokharel <76983858+ganeshpokharel007@users.noreply.github.com>
2026-04-19 08:48:04 +0100

    Update model.py

--- model.py
+++ model.py
@@ -11,12 +11,12 @@ X = data["text"]
 y = data["category"]
 
 # Convert text to numbers
-vectorizer = CountVectorizer()
+vectorizer = CountVectorizer(stop_words='english')
+
 X_vec = vectorizer.fit_transform(X)
 
 # Split data
-X_train, X_test, y_train, y_test = train_test_split(X_vec, y, test_size=0.2)
-
+X_train, X_test, y_train, y_test = train_test_split(X_vec, y, test_size=0.2, random_state=42)
 # Train model
 model = MultinomialNB()
 model.fit(X_train, y_train)
